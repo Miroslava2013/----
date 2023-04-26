88e1204a765ea97f57a78be81115f81676ab3dec
Miroslava2013 <131914327+Miroslava2013@users.noreply.github.com>
2023-04-26 21:53:37 +0200

    Початкові файли для проекту MakeCode

---
+++ main.ts
@@ -0,0 +1,42 @@
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    # . . # .
+    # # # # .
+    . # . # .
+    # . . # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . #
+    . # . # .
+    . # # . .
+    . # . # .
+    . # . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . # # #
+    # . # . #
+    # # # . #
+    # . # . #
+    # . # # #
+    `)
+basic.pause(1000)
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
